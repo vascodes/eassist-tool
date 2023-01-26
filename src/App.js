e50a92e5afb59e57b27f9a7dcca4e22703daa313
vascodes <nikhilvas12@gmail.com>
2023-01-26 23:02:41 +0530

    Add fetchData() to App.js to fetch initial data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import QuestionBlock from "./components/QuestionBlock";
 
 function App() {
+	const [content, setContent] = useState(null);
+
+	async function fetchData() {
+		try {
+			const response = await fetch("http://localhost:8000/e-assist");
+			const responseJSON = await response.json();
+			setContent(responseJSON);
+		} catch (error) {
+			console.log("Error! Could not fetch data." + error);
+		}
+	}
+
+	useEffect(() => fetchData, []);
+
 	return (
 		<div className="app-container">
-			<div className="questions-container">
-				<QuestionBlock questionText="Sample Question" />
-			</div>
+			<div className="questions-container">{content && <QuestionBlock questionText="Sample Question" />}</div>
 		</div>
 	);
 }
