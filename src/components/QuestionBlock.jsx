cffdaa22db28a7f79fb02f88662138f845c861bf
vascodes <nikhilvas12@gmail.com>
2023-01-27 13:42:34 +0530

    Update QuestionBlock component to show substances and corresponding options.

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -2,18 +2,45 @@ function QuestionBlock({ substances, question }) {
 	return (
 		<div className="question-block">
 			<p className="question-text">{question.text}</p>
-			{substances.map((substance) => (
-				<div
-					key={substance.id}
-					className="option-block"
-				>
-					<p className="option-text">
-						{substance.name}
-						{substance.examples && ` (${substance.examples} etc.)`}
-					</p>
-					<div className="options"></div>
-				</div>
-			))}
+			{substances.map((substance) => {
+				const substanceName = substance.name;
+				const examples = substance.examples;
+
+				return (
+					<div
+						key={substance.id}
+						className="option-block"
+					>
+						<p className="option-text">
+							{substanceName}
+							{examples && ` (${examples} etc.)`}
+						</p>
+
+						<div className="options">
+							{question.options?.map((option) => {
+								const shortSubstanceName = substanceName.replace(" ", "").toLowerCase();
+								const optionName = `radio-${shortSubstanceName}`;
+								const optionId = `radio-${shortSubstanceName}-${option.toLowerCase()}`;
+
+								return (
+									<div
+										key={option}
+										className="option-group"
+									>
+										<input
+											type="radio"
+											className="option-radio"
+											name={optionName}
+											id={optionId}
+										/>
+										<label htmlFor={optionId}>{option}</label>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
