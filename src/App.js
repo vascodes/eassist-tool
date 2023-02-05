d283f189ed831a6474247b25a2bc5adc4a4b03f3
vascodes <nikhilvas12@gmail.com>
2023-02-06 00:42:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import Question from "./components/Question";
 
 function App() {
 	const [content, setContent] = useState(null);
-	const [showCategorySelect, setShowCategorySelect] = useState(false);
 	const [showQuestions, setShowQuestions] = useState(true);
 	const [currentQuestionNumber, setCurrentQuestionNumber] = useState(1);
 	const [showPrevButton, SetShowPrevButton] = useState(false);
 	const [selectedOptions, setSelectedOptions] = useState({});
+	const [selectedSubstances, setSelectedSubstances] = useState([]);
 
 	const questions = content?.questions;
 	const questionId = "question" + currentQuestionNumber; //ex: question5
 	const currentQuestion = questions ? questions[questionId] : {};
 	const substances = currentQuestion?.substances;
 
-	function handleNextButtonClick() {
-		setShowCategorySelect(false);
-		setShowQuestions(true);
+	const CategoryScores = {
+		tobacco: 0,
+		alcohol: 0,
+		cannabis: 0,
+		cocaine: 0,
+		amphetamine: 0,
+		inhalants: 0,
+		sedatives: 0,
+		hallucinogens: 0,
+		opioids: 0,
+		other: 0,
+	};
 
+	function handleNextButtonClick() {
 		setCurrentQuestionNumber((prevNo) => {
 			let totalQuestions = Object.keys(questions).length;
 
@@ -35,8 +45,6 @@ function App() {
 
 	function togglePrevButton(currentQuestionNumber) {
 		if (currentQuestionNumber === 1) {
-			// setShowQuestions(false); // UNCOMMENT LATER
-			setShowCategorySelect(true);
 			SetShowPrevButton(false);
 		} else {
 			SetShowPrevButton(true);
@@ -66,23 +74,19 @@ function App() {
 			};
 
 			newSelectedOptions[questionId].substances[substanceId] = optionText;
-
 			return newSelectedOptions;
 		});
 	}
 
 	useEffect(() => setContent(data), []); // Fetch data on app load.
 	useEffect(() => togglePrevButton(currentQuestionNumber), [currentQuestionNumber]);
-	// useEffect(() => console.log(selectedOptions), [selectedOptions]);
 
 	return (
 		<div className="app-container">
-			{content && (showQuestions || showCategorySelect) && (
-				<div className="navigation">
-					{showPrevButton && <button onClick={() => handlePrevButtonClick()}>Previous</button>}
-					<button onClick={() => handleNextButtonClick()}>Next</button>
-				</div>
-			)}
+			<div className="navigation">
+				{showPrevButton && <button onClick={() => handlePrevButtonClick()}>Previous</button>}
+				<button onClick={() => handleNextButtonClick()}>Next</button>
+			</div>
 
 			<div className="questions-container">
 				{content && showQuestions && (
